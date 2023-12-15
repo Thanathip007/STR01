e84edf7ecaa84c9da67f7be31c0f8555e09fcda5
Thanathip007 <644230014@webmail.npru.ac.th>
2023-12-15 14:32:47 +0700

    .commit

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./picture/mypic.jfif')
+    st.image('./picture/mypic.jpg')
 with col2:
     st.image('./picture/iris.jpg')
 
